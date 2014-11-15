13677f5bb12310c878606b97662bb403f82457c5
Jacques Lucke <mail@jlucke.com>
2014-11-15 21:27:23 +0100

    conf.py update

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ templates_path = ['ytemplates']
 source_suffix = '.rst'
 master_doc = 'index'
 
-project = u'Animation Nodes Documentation'
+project = u'Animation Nodes'
 copyright = u'2014, Jacques Lucke'
 
 version = '0.0.1'
@@ -23,36 +23,27 @@ html_theme = 'default'
 
 html_static_path = ['ystatic']
 
-htmlhelp_basename = 'AnimationNodesDocumentationdoc'
+htmlhelp_basename = 'AnimationNodesdoc'
 
 
 latex_elements = {}
 latex_documents = [
-  ('index', 'AnimationNodesDocumentation.tex', u'Animation Nodes Documentation Documentation',
+  ('index', 'AnimationNodes.tex', u'Animation Nodes Documentation',
    u'Jacques Lucke', 'manual'),
 ]
 
 man_pages = [
-    ('index', 'animationnodesdocumentation', u'Animation Nodes Documentation Documentation',
+    ('index', 'animationnodes', u'Animation Nodes Documentation',
      [u'Jacques Lucke'], 1)
 ]
 texinfo_documents = [
-  ('index', 'AnimationNodesDocumentation', u'Animation Nodes Documentation Documentation',
-   u'Jacques Lucke', 'AnimationNodesDocumentation', 'One line description of project.',
+  ('index', 'AnimationNodes', u'Animation Nodes Documentation',
+   u'Jacques Lucke', 'AnimationNodes', 'A new way to work with data in Blender.',
    'Miscellaneous'),
 ]
 
-epub_title = u'Animation Nodes Documentation'
+epub_title = u'Animation Nodes'
 epub_author = u'Jacques Lucke'
 epub_publisher = u'Jacques Lucke'
 epub_copyright = u'2014, Jacques Lucke'
 epub_exclude_files = ['search.html']
-
-# Scale large images.
-#epub_max_image_width = 0
-
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#epub_show_urls = 'inline'
-
-# If false, no index is generated.
-#epub_use_index = True
