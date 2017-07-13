69fedb1c7baa798f9d0c6207e5f397c7684d6cfa
Jacques Lucke <mail@jlucke.com>
2017-07-13 18:43:18 +0200

    update config

--- docs/conf.py
+++ docs/conf.py
@@ -18,10 +18,10 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 project = u'Animation Nodes'
-copyright = u'2015, Jacques Lucke'
+copyright = u'2017, Jacques Lucke'
 
-version = '0.0.1'
-release = '0.0.1'
+version = '2.0'
+release = '2.0'
 
 exclude_patterns = ['includes/*']
 
@@ -31,22 +31,22 @@ htmlhelp_basename = 'AnimationNodesdoc'
 
 latex_elements = {}
 latex_documents = [
-  ('index', 'AnimationNodes.tex', u'Animation Nodes Documentation',
+  ('index', 'AnimationNodes.tex', u'Animation Nodes Manual',
    u'Jacques Lucke', 'manual'),
 ]
 
 man_pages = [
-    ('index', 'animationnodes', u'Animation Nodes Documentation',
+    ('index', 'animation_nodes', u'Animation Nodes Manual',
      [u'Jacques Lucke'], 1)
 ]
 texinfo_documents = [
-  ('index', 'AnimationNodes', u'Animation Nodes Documentation',
-   u'Jacques Lucke', 'AnimationNodes', 'A new way to work with data in Blender.',
+  ('index', 'AnimationNodes', u'Animation Nodes Manual',
+   u'Jacques Lucke', 'AnimationNodes', 'Node based visual scripting system designed for motion graphics in Blender.',
    'Miscellaneous'),
 ]
 
 epub_title = u'Animation Nodes'
 epub_author = u'Jacques Lucke'
 epub_publisher = u'Jacques Lucke'
-epub_copyright = u'2014, Jacques Lucke'
+epub_copyright = u'2017, Jacques Lucke'
 epub_exclude_files = ['search.html']
